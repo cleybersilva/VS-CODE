04d5f2deb3e30f9028c08a4ceae961edd49f836c
Cleyber Silva <cleyber.silva@live.com>
2022-06-17 13:01:34 -0300

    Inagem

--- DIO/flashlight/App.js
+++ DIO/flashlight/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, image} from 'react-native';
+import imagex from './assets/icons/eco-light-off.png';
 
 const App = () => {
-  return <View />;
+  const toggle = true; //false
+
+  // if toggle retorn light
+  return (
+     <View style={toggle ? style.containerLight : style.container}>
+    <Image source={imagex} />
+    </view>
+  );
 };
 
 export default App;
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerLights: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
