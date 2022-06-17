d5a98d58c0431e43fed6406f37a3dbe3ca9e217d
Cleyber Silva <cleyber.silva@live.com>
2022-06-17 12:10:13 -0300

    React-Native

---
+++ Dashboard VENI/js/login.js
@@ -0,0 +1,19 @@
+var x = document.getElementById("login");
+var y = document.getElementById("senha");
+var z = document.getElementById("btn");
+
+function senha() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
+let button = document.querySelector('form button.button')
+button.addEventListener('click', () => {
+    location.href = "dashboard.html"
+})
+
