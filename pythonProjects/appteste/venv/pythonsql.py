44bb4d9f353d5c92a8e012c65317ddb91ccd0178
cleybersilva <cleyber.silva@live.com>
2023-10-29 20:15:43 -0300

    conexao

--- pythonProjects/appteste/venv/pythonsql.py
+++ pythonProjects/appteste/venv/pythonsql.py
@@ -8,3 +8,11 @@ dados_conexao = (
 
 conexao = pyodbc.connect(dados_conexao)
 print("Conexão Bem Sucedida!")
+
+cursor = conexao.cursor()
+
+comando = """INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade)
+VALUES
+    (1, 'Lira', 'PC', '15/02/2021', 8000, 1)"""
+
+cursor.execute(comando)
