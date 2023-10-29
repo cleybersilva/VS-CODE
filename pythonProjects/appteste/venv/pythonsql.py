ecd06b5085b7af1919614e746aa63184cf3b391c
cleybersilva <cleyber.silva@live.com>
2023-10-29 20:18:10 -0300

    conexao2

--- pythonProjects/appteste/venv/pythonsql.py
+++ pythonProjects/appteste/venv/pythonsql.py
@@ -13,6 +13,7 @@ cursor = conexao.cursor()
 
 comando = """INSERT INTO Vendas(id_venda, cliente, produto, data_venda, preco, quantidade)
 VALUES
-    (1, 'Lira', 'PC', '15/02/2021', 8000, 1)"""
+    (2, 'Juci', 'Iphone', '29/10/2023', 8000, 1)"""
 
 cursor.execute(comando)
+cursor.commit()
