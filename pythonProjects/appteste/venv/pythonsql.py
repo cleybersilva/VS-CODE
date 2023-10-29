228c1ff211efe6679b6eeb96a31d5e6e41b77307
cleybersilva <cleyber.silva@live.com>
2023-10-29 19:50:09 -0300

    oK

---
+++ pythonProjects/appteste/venv/pythonsql.py
@@ -0,0 +1,10 @@
+import pyodbc
+
+dados_conexao (
+    "Driver={SQL Server};"
+    "Server=cleyber;"
+    "Database=PythonSQL;"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+print("Conexão Bem Sucedida!")
