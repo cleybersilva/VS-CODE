9bf1bb4ba5afd591bfa37e48c3b0a46c1254af8f
cleybersilva <cleyber.silva@live.com>
2023-10-29 21:09:03 -0300

    excluir

--- pythonProjects/appteste/appteste/urls.py
+++
@@ -1,9 +0,0 @@
-##Teste
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('clientes/', include('clientes.urls'))
-]
